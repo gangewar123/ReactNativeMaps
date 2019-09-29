c706089a98ee8938856581ccf4281746027ae4ba
kiran kumar bv <30715570+KiranChimple@users.noreply.github.com>
2019-09-29 11:50:36 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,157 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Fragment } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar, Dimensions
+} from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+
+
+
+
+    <View style={styles.container} >
+      <MapView style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation={true}
+        showsMyLocationButton={false}
+        zoomEnabled={true}
+        region={{
+          latitude: 42.882004, longitude: 74.582748,
+          latitudeDelta: 0.0922, longitudeDelta: 0.0421
+        }} />
+
+    </View>
+
+
+    // <Fragment>
+    //   <StatusBar barStyle="dark-content" />
+    //   <SafeAreaView>
+    //     <ScrollView
+    //       contentInsetAdjustmentBehavior="automatic"
+    //       style={styles.scrollView}>
+
+    //       <Header />
+    //       {global.HermesInternal == null ? null : (
+    //         <View style={styles.engine}>
+    //           <Text style={styles.footer}>Engine: Hermes</Text>
+    //         </View>
+    //       )}
+    //       <View style={styles.body}>
+    //         <View style={styles.sectionContainer}>
+    // <MapView style={{ flex: 1 }}
+    //   region={{
+    //     latitude: 42.882004, longitude: 74.582748,
+    //     latitudeDelta: 0.0922, longitudeDelta: 0.0421
+    //   }}
+    //   showsUserLocation={true} />
+    //         </View>
+    //         <View style={styles.sectionContainer}>
+    //           <Text style={styles.sectionTitle}>See Your Changes</Text>
+    //           <Text style={styles.sectionDescription}>
+    //             <ReloadInstructions />
+    //           </Text>
+    //         </View>
+    //         <View style={styles.sectionContainer}>
+    //           <Text style={styles.sectionTitle}>Debug</Text>
+    //           <Text style={styles.sectionDescription}>
+    //             <DebugInstructions />
+    //           </Text>
+    //         </View>
+    //         <View style={styles.sectionContainer}>
+    //           <Text style={styles.sectionTitle}>Learn More</Text>
+    //           <Text style={styles.sectionDescription}>
+    //             Read the docs to discover what to do next:
+    //           </Text>
+    //         </View>
+    //         <LearnMoreLinks />
+    //       </View>
+    //     </ScrollView>
+    //   </SafeAreaView>
+    // </Fragment>
+  );
+};
+var { width, height } = Dimensions.get('window')
+
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+    flex: 1,
+    backgroundColor: "green",
+    width: width,
+    height: height,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
